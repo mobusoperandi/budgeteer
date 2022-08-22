71619866d4b5ece36d4f24d5e5b16b1b4adb37bf
Shahar Dawn Or <mightyiampresence@gmail.com>
2022-08-22 19:48:27 +0700

    test: introduce trycmd
    Co-authored-by: Vimal Patel <mailtovimal@gmail.com>
    Co-authored-by: Alexander Nitiola <cre8tor.alexander@gmail.com>

--- tests/test.rs
+++ tests/test.rs
@@ -3,15 +3,19 @@ use std::{path::PathBuf, process::Command};
 use tempfile::tempdir;
 
 const BIN_PATH: &str = env!(concat!("CARGO_BIN_EXE_", env!("CARGO_PKG_NAME")));
+const PERSISTANCE_FILE: &str = "PERSISTANCE_FILE";
 
 fn make_command_sans_persistance_file() -> (PathBuf, Command) {
-    let temp_dir_path = tempdir().unwrap().into_path();
-    let persistance_file_path = temp_dir_path.join("pf");
+    let persistance_file_path = path_to_non_existant_file();
 
     let mut command = Command::new(BIN_PATH);
-    command.env("PERSISTANCE_FILE", &persistance_file_path);
+    command.env(PERSISTANCE_FILE, &persistance_file_path);
 
     (persistance_file_path, command)
+}
+
+fn path_to_non_existant_file() -> PathBuf {
+    tempdir().unwrap().into_path().join("non_existant_file")
 }
 
 #[test]
@@ -32,4 +36,12 @@ fn if_persistance_file_doesnt_exist_it_is_created() {
 fn if_persistance_file_env_not_defined_exit_with_non_zero() {
     let status = Command::new(BIN_PATH).status().unwrap();
     assert_ne!(status.code().unwrap(), 0);
+}
+
+#[test]
+fn readme() {
+    trycmd::TestCases::new().case("README.md").env(
+        PERSISTANCE_FILE,
+        path_to_non_existant_file().to_str().unwrap(),
+    );
 }
