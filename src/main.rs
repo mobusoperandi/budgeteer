f91f2e5c007994128a021d812f02c4dc3bd8812e
Shahar Dawn Or <mightyiampresence@gmail.com>
2022-08-28 15:51:09 +0700

    refactor: persistance typo fixed

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 use std::{env, fs};
 
 fn main() {
-    let persistance_file_path = env::var("PERSISTANCE_FILE").unwrap();
+    let persistence_file_path = env::var("PERSISTENCE_FILE").unwrap();
 
-    fs::File::create(persistance_file_path).unwrap();
+    fs::File::create(persistence_file_path).unwrap();
 }
--- tests/test.rs
+++ tests/test.rs
@@ -3,15 +3,15 @@ use std::{path::PathBuf, process::Command};
 use tempfile::tempdir;
 
 const BIN_PATH: &str = env!(concat!("CARGO_BIN_EXE_", env!("CARGO_PKG_NAME")));
-const PERSISTANCE_FILE: &str = "PERSISTANCE_FILE";
+const PERSISTENCE_FILE: &str = "PERSISTENCE_FILE";
 
-fn make_command_sans_persistance_file() -> (PathBuf, Command) {
-    let persistance_file_path = path_to_non_existant_file();
+fn make_command_sans_persistence_file() -> (PathBuf, Command) {
+    let persistence_file_path = path_to_non_existant_file();
 
     let mut command = Command::new(BIN_PATH);
-    command.env(PERSISTANCE_FILE, &persistance_file_path);
+    command.env(PERSISTENCE_FILE, &persistence_file_path);
 
-    (persistance_file_path, command)
+    (persistence_file_path, command)
 }
 
 fn path_to_non_existant_file() -> PathBuf {
@@ -20,20 +20,20 @@ fn path_to_non_existant_file() -> PathBuf {
 
 #[test]
 fn an_executable_named_after_the_package_exits_with_zero() {
-    let (_, mut command) = make_command_sans_persistance_file();
+    let (_, mut command) = make_command_sans_persistence_file();
     assert!(command.status().unwrap().success());
 }
 
 #[test]
-fn if_persistance_file_doesnt_exist_it_is_created() {
-    let (persistance_file_path, mut command) = make_command_sans_persistance_file();
+fn if_persistence_file_doesnt_exist_it_is_created() {
+    let (persistence_file_path, mut command) = make_command_sans_persistence_file();
     let status = command.status().unwrap();
     assert!(status.success());
-    assert!(persistance_file_path.exists());
+    assert!(persistence_file_path.exists());
 }
 
 #[test]
-fn if_persistance_file_env_not_defined_exit_with_non_zero() {
+fn if_persistence_file_env_not_defined_exit_with_non_zero() {
     let status = Command::new(BIN_PATH).status().unwrap();
     assert_ne!(status.code().unwrap(), 0);
 }
@@ -42,7 +42,7 @@ fn if_persistance_file_env_not_defined_exit_with_non_zero() {
 #[should_panic]
 fn readme() {
     trycmd::TestCases::new().case("README.md").env(
-        PERSISTANCE_FILE,
+        PERSISTENCE_FILE,
         path_to_non_existant_file().to_str().unwrap(),
     );
 }
